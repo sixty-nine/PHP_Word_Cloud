8b515bdb58b0e4204eea730ea1b53930c1e4c3e5
Dan <dan@dc-home.ch>
2010-09-15 11:41:26 +0200

    Extracted the hardcoded frequency for vertical words

--- tagcloud.php
+++ tagcloud.php
@@ -145,6 +145,12 @@ class Mask {
  */
 class FrequencyTable {
 
+  const WORDS_HORIZONTAL = 0;
+  const WORDS_MAINLY_HORIZONTAL = 1;
+  const WORDS_MIXED = 6;
+  const WORDS_MAINLY_VERTICAL = 9;
+  const WORDS_VERTICAL = 10;
+
   private $table = array();
   private $rejected_words = array(
     'and', 'our', 'your', 'their', 'his', 'her', 'the', 'you', 'them', 'yours',
@@ -155,13 +161,14 @@ class FrequencyTable {
    * Construct a new FrequencyTable from a word list and a font 
    * @param string $text The text containing the words
    * @param string $font The TTF font file
+   * @param integer $vertical_freq Frequency of vertical words (0 - 10, 0 = All horizontal, 10 = All vertical)
    */
-  public function __construct($text, $font) {
+  public function __construct($text, $font, $vertical_freq = FrequencyTable::WORDS_MAINLY_HORIZONTAL) {
   
     $this->font = $font;
     $words = split("[\n\r\t ]+", $text);
     $this->create_frequency_table($words);
-    $this->process_frequency_table();
+    $this->process_frequency_table($vertical_freq);
   }
 
   /**
@@ -199,16 +206,16 @@ class FrequencyTable {
   
   /**
    * Calculate word frequencies and set additionnal properties of the frequency table
+   * @param integer $vertical_freq Frequency of vertical words (0 - 10, 0 = All horizontal, 10 = All vertical)
    */
-  private function process_frequency_table() {
+  private function process_frequency_table($vertical_freq = FrequencyTable::WORDS_MAINLY_HORIZONTAL) {
     $count = count($this->table);
     foreach($this->table as $key => $val) {
       $f = $this->table[$key]->count / $count;
       $this->table[$key]->size = (integer)(500 * $f) + 7;
       $this->table[$key]->size += rand(-5, 3); // Add some noize to the font sizes
       $this->table[$key]->angle = 0;
-      // TODO: Extract this hardcoded frequency of vertical words
-      if (rand(1, 10) == 1) $this->table[$key]->angle = 90;
+      if (rand(1, 10) <= $vertical_freq) $this->table[$key]->angle = 90;
       $this->table[$key]->box = imagettfbbox ($this->table[$key]->size, $this->table[$key]->angle, $this->font, $key);
     }
   }
